d4ef42c71c7cbed92a05bf6ba6d1f32ae4925b98
github_dineshreddy9 <tdineshreddy9@gmail.com>
2018-05-01 13:00:37 -0500

    first commit

---
+++ spark.py
@@ -0,0 +1,86 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.streaming import StreamingContext
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import json
+import re
+from elasticsearch import Elasticsearch
+import time
+
+TCP_IP = 'localhost'
+TCP_PORT = 9001
+
+def sentimentExtract(text):
+    senti = SentimentIntensityAnalyzer()
+    polarity = senti.polarity_scores(text)
+    if(polarity["compound"] > 0):
+        return "positive"
+    elif(polarity["compound"] < 0):
+        return "negative"
+    else:
+        return "neutral"
+
+# Pyspark
+# create spark configuration
+conf = SparkConf()
+conf.setAppName('TwitterApp')
+conf.setMaster('local[2]')
+# create spark context with the above configuration
+sc = SparkContext(conf=conf)
+
+# create the Streaming Context from spark context with interval size 2 seconds
+ssc = StreamingContext(sc, 2)
+ssc.checkpoint("checkpoint_TwitterApp")
+# read data from port 9001
+dataStream = ssc.socketTextStream(TCP_IP, TCP_PORT)
+dataStream.pprint()
+es = Elasticsearch()
+ts = time.time() 
+tsmilli = ts*1000
+es_conf = { "es.resource" : "tweet/tweetDetails", "es.nodes":"localhost", "es.port":"9200"} 
+dstreamjson = dataStream.map(lambda line: json.loads(line)).map(lambda line: {"text": line["cleaned"], "sentiment": sentimentExtract(line["cleaned"]), "location":{"lat": line["loc"].split("::::")[0], "lon": line["loc"].split("::::")[1]}, "timestamp":tsmilli})
+dstreamjson2 = dstreamjson.map(lambda x : ("a", x))
+dstreamjson2.foreachRDD(lambda line : line.saveAsNewAPIHadoopFile(
+            path="-", 
+            outputFormatClass="org.elasticsearch.hadoop.mr.EsOutputFormat",
+            keyClass="org.apache.hadoop.io.NullWritable", 
+            valueClass="org.elasticsearch.hadoop.mr.LinkedMapWritable", 
+            conf=es_conf))
+            
+
+######### your processing here ###################
+dstreamjson2.pprint()
+
+#################################################
+
+ssc.start()
+ssc.awaitTermination()
+
+'''
+PUT tweet
+{
+  "mappings": {
+    "tweetDetails": {
+      "properties": {
+        "sentiment": {
+          "type": "text"
+        },
+        "location": {
+          "type": "geo_point"
+        },
+        "timestamp": {
+          "type": "date",
+          "format": "epoch_millis"
+        },
+        "text": {
+          "type": "text",
+          "fields":{
+            "keyword": {
+                "type" : "keyword"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+'''
---
+++ stream.py
@@ -0,0 +1,71 @@
+import json 
+import tweepy
+import socket
+import sys
+import re
+import requests
+
+ACCESS_TOKEN = '982681246181535744-Vr5O9fJLi0kUWmObaVAxj45SRnRCBsX'
+ACCESS_SECRET = 'i5ief1Q47gPj0dOVxV3tfwPRg9y89M3ARVHxIBxm4sNg3'
+CONSUMER_KEY = 'nI2r6r3nouJ1VelimiLD8Edb5'
+CONSUMER_SECRET = 'HrYEbfODrOiYc9ftNWP852XTp6FavuIhCdSMRE5fGvFTH2I3FB'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+
+hashtag = '#'+sys.argv[1]
+
+
+TCP_IP = 'localhost'
+TCP_PORT = 9001
+
+
+# create sockets
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.connect((TCP_IP, TCP_PORT))
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+#conn, addr = s.accept()
+
+def getLoc(address):
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyCycfZdWxulqeK8aatreOFFIvYVF5ucCgM"
+    r = requests.get(url)
+    # extracting data in json format
+    data = r.json()
+    if data["results"] is not None and len(data["results"]) > 0:
+        return str(data["results"][0]["geometry"]["location"]["lat"])+"::::"+str(data["results"][0]["geometry"]["location"]["lng"])
+    else:
+        return None
+ 
+
+class MyStreamListener(tweepy.StreamListener):
+    def on_data(self, status):
+        out = json.loads(status)
+        #print(out["text"])
+        rawlocation = out["user"]["location"]
+        if hashtag in out["text"]:
+          print(out["text"])
+          if rawlocation is not None:
+             cleaned  = ' '.join(re.sub("(@+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", out["text"]).split()) + '\n'
+             jsonob = { "cleaned":cleaned}
+             print(jsonob["cleaned"])
+             loc = getLoc(rawlocation)
+             jsonob["loc"] = loc
+             if loc is not None:
+                conn, addr = s.accept()
+                djsonob=json.dumps(jsonob)
+                conn.send(djsonob.encode('utf-8'))
+                conn.close()
+    
+    def on_error(self, status_code):
+        if status_code == 420:
+            return False
+        else:
+            print(status_code)
+
+myStream = tweepy.Stream(auth=auth, listener=MyStreamListener())
+
+myStream.filter(track=[hashtag])
+
+
